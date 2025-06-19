080be7ec3674d23346538b676859c2dd15aec27e
Itay Raviv <itayraviv5@gmail.com>
2025-06-19 14:28:06 +0300

    exex moitoring work end to end

--- cmd/main.go
+++ cmd/main.go
@@ -7,9 +7,18 @@ import (
 
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/link"
+	"golang.org/x/sys/unix"
 )
 
 func main() {
+	//allow unlimited locked memory
+	if err := unix.Setrlimit(unix.RLIMIT_MEMLOCK, &unix.Rlimit{
+		Cur: unix.RLIM_INFINITY,
+		Max: unix.RLIM_INFINITY,
+	}); err != nil {
+		log.Fatalf("failed to adjust rlimit: %v", err)
+	}
+
 	// Load precompiled BPF object file
 	spec, err := ebpf.LoadCollectionSpec("exec_monitor.bpf.o")
 	if err != nil {
